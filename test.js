24fb0ee822b52b8ecda4ba9392564f6fb6ebc8b6
Karan Dalvi <karan.j.dalvi@gmail.com>
2017-04-18 00:45:08 -0400

    Socket Communication

--- test.js
+++ test.js
@@ -1,38 +1,29 @@
-function sendResp(req, resp) {
 var oracledb = require('oracledb');
 oracledb.getConnection(
   {
-    user          : "",
-    password      : "",
+    user          : "kdalvi",
+    password      : "DBMSsp17",
     connectString : "oracle.cise.ufl.edu/orcl"
   },
   function(err, connection)
   {
     if (err) {
       console.error(err.message);
-      resp.write(err.message);
-      resp.end();
       return;
     }
     connection.execute(
-      "SELECT name " +
+      "SELECT name, city " +
         "FROM branch " +
         "WHERE 2=2",
         {}, //no binds
-        {
-          outFormat: oracledb.OBJECT
-        },
-      // [110],  // bind value for :id
       function(err, result)
       {
         if (err) {
           console.error(err.message);
-          resp.write(err.message);
-          resp.end();
           doRelease(connection);
           return;
         }
-        console.log(result[0]);
+        console.log(result.rows);
         doRelease(connection);
       });
   });
@@ -45,11 +36,3 @@ function doRelease(connection)
         console.error(err.message);
     });
 }
-}
-
-var http = require('http');
-var port = 8080;
-var httpServer = http.createServer(function(req, resp) {
-  sendResp(req,resp);
-});
-httpServer.listen(port);
